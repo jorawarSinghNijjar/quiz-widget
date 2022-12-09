ad3830e22516c56af8c18a6eb952a67a38f8cbb4
Jorawar Singh <jorawarsinghnijjar@gmail.com>
2022-12-09 16:20:37 -0500

    add zest ec2 address

--- src/apiUrlConfig.js
+++ src/apiUrlConfig.js
@@ -1,3 +1,3 @@
 export const devUrl = "http://localhost:3300";
 // export const prodUrl = "https://ec2-18-118-34-240.us-east-2.compute.amazonaws.com:3300"
-export const prodUrl = "https://zest-mail.link"
+export const prodUrl = "http://ec2-18-191-54-176.us-east-2.compute.amazonaws.com:3300/"
