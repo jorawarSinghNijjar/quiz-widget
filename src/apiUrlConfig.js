cfd98d9a5b9d968fa37d5dc913b64160fd1dad41
Jorawar Singh <jorawarsinghnijjar@gmail.com>
2022-12-09 16:16:09 -0500

    gh pages commit

--- src/apiUrlConfig.js
+++ src/apiUrlConfig.js
@@ -1,3 +1,3 @@
 export const devUrl = "http://localhost:3300";
 // export const prodUrl = "https://ec2-18-118-34-240.us-east-2.compute.amazonaws.com:3300"
-export const prodUrl = "http://zest-mail.link"
+export const prodUrl = "https://zest-mail.link"
